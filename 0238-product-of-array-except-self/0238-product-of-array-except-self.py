50b52ea9cb77171db689122c20ac2c7875e0b097
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2025-04-28 19:34:41 -0400

    Time: 23 ms (63.95%), Space: 23.9 MB (37.82%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,18 +1,10 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        total = 1
-        total_zeroes = 0
-        for num in nums:
-            if num != 0: total *= num
-            total_zeroes += num == 0
-        output = [0] * len(nums)
-        if total_zeroes >= 2:
-            return output
-        for i,num in enumerate(nums):
-            if num == 0:
-                output[i] = total
-            elif num != 0 and not total_zeroes:
-                output[i] = total // num
-
-        return output
-
+        output = [1] * (len(nums) + 1)
+        for i in range(len(nums)):
+            output[i + 1] = output[i] * nums[i]
+        back = 1
+        for i in range(len(nums) - 1,-1,-1):
+            output[i + 1] = back * output[i]
+            back *= nums[i]
+        return output[1:]
