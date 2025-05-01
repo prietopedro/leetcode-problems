cc562b16df683143375b39cde054a8737cea1010
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2025-04-30 22:32:13 -0400

    Time: 533 ms (24.76%), Space: 26.1 MB (58.93%) - LeetHub

--- 2071-maximum-number-of-tasks-you-can-assign/2071-maximum-number-of-tasks-you-can-assign.py
+++ 2071-maximum-number-of-tasks-you-can-assign/2071-maximum-number-of-tasks-you-can-assign.py
@@ -1,33 +1,5 @@
 class Solution:
     def maxTaskAssign(self, tasks: List[int], workers: List[int], pills: int, strength: int) -> int:
-        # WHY DONT THIS WORK???!!!
-        # tasks.sort()
-        # workers.sort()
-
-        # def can_complete(m):
-        #     task_index = 0
-        #     pills_remaining = pills 
-        #     new_workers = [(s,0) for s in workers[len(workers) - m:]]
-        #     for i in range(m):
-        #         while new_workers[0][0] < tasks[i]:
-        #             worker_strength,used = heapq.heappop(new_workers)
-        #             if not pills_remaining or used: return False
-        #             heapq.heappush(new_workers,(worker_strength + strength,1))
-        #             pills_remaining -= 1
-        #         heapq.heappop(new_workers)
-
-        #     return True
-
-        # low = 1
-        # high = min(len(tasks),len(workers)) + 1
-        # while low < high:
-        #     middle = (low + high) // 2
-        #     print(middle,can_complete(middle))
-        #     if not can_complete(middle):
-        #         high = middle
-        #     else:
-        #         low = middle + 1
-        # return low - 1
         n, m = len(tasks), len(workers)
         tasks.sort()
         workers.sort()
