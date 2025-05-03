86bad9beb995b2dd55f80e49199dce5731ed3366
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2025-05-02 20:32:52 -0400

    Time: 106 ms (12.45%), Space: 18.4 MB (66.24%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.py
@@ -4,23 +4,17 @@ class Solution:
         curr_top_inverse,curr_bot_inverse = bottoms[0],tops[0]
         top_counter,bot_counter = 0,0
         top_counter_inverse,bot_counter_inverse = 0,0
+        def count(num,inverse,against):
+            if num == against:
+                return 0
+            if inverse != against:
+                return inf
+            return 1
         for num1,num2 in zip(tops,bottoms):
-            if num1 != curr_top:
-                if num2 != curr_top:
-                    top_counter = inf
-                top_counter += 1
-            if num2 != curr_bot:
-                if num1 != curr_bot:
-                    bot_counter = inf
-                bot_counter += 1
-            if num1 != curr_top_inverse:
-                if num2 != curr_top_inverse:
-                    top_counter_inverse = inf
-                top_counter_inverse += 1
-            if num2 != curr_bot_inverse:
-                if num1 != curr_bot_inverse:
-                    bot_counter_inverse = inf
-                bot_counter_inverse += 1
+            top_counter += count(num1,num2,curr_top)
+            bot_counter += count(num2,num1,curr_bot)
+            top_counter_inverse += count(num1,num2,curr_top_inverse)
+            bot_counter_inverse += count(num2,num1,curr_bot_inverse)
         best = min(top_counter,bot_counter,top_counter_inverse,bot_counter_inverse)
         return best if best != inf else -1
     
