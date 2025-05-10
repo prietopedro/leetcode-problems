c30aaaf975e9f5ae4227ccc4b7f6ddf438f06136
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2025-05-09 20:23:55 -0400

    Time: 864 ms (42.21%), Space: 35 MB (42.88%) - LeetHub

--- 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.py
+++ 2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros/2918-minimum-equal-sum-of-two-arrays-after-replacing-zeros.py
@@ -4,7 +4,6 @@ class Solution:
         num1_zeroes,num2_zeroes = sum(not x for x in nums1),sum(not x for x in nums2)
         if num1_sum == num2_sum and num1_zeroes + num2_zeroes == 0:
             return num1_sum
-        print(num2_sum - num1_sum + num2_zeroes)
         if num2_sum + num2_zeroes >= num1_sum + num1_zeroes and num2_sum - num1_sum + num2_zeroes >= num1_zeroes and num1_zeroes:
             return num2_sum + num2_zeroes
         if num1_sum + num1_zeroes >= num2_sum + num2_zeroes and num1_sum - num2_sum + num1_zeroes >= num2_zeroes and num2_zeroes:
