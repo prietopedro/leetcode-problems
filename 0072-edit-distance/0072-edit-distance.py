7e1f0213eab3004a9d1baa94dca7012f6b8c35cf
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2025-05-28 19:41:08 -0400

    Time: 224 ms (5.03%), Space: 51.9 MB (5.01%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        m,n = len(word1),len(word2)
+        @cache
+        def rec(i,j):
+            left_i,left_j = m - i,n - j
+            if i >= m:
+                return max(left_j,0)
+            if j >= n:
+                return max(left_i,0)
+
+            middle = (word1[i] != word2[j]) + rec(i + 1,j + 1)
+            left = 1 + rec(i + 1,j)
+            right = 1 + rec(i,j + 1)
+            return min(left,right,middle)
+        return rec(0,0)
