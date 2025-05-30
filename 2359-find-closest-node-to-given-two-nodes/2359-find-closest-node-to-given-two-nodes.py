28ee5e0c119e84bde0695db9c4917dcba0d3e68e
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2025-05-29 21:41:25 -0400

    Time: 32 ms (98.19%), Space: 32.2 MB (81.9%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
@@ -1,25 +1,19 @@
 class Solution:
     def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
-        node1_q = deque([node1])
-        node2_q = deque([node2])
         node1_visited = set()
         node2_visited = set()
-        while node1_q or node2_q:
+        while node1 is not None or node2 is not None:
             best = inf
-            for _ in range(len(node1_q)):
-                node = node1_q.popleft()
-                node1_visited.add(node)
-                if node in node2_visited:
-                    best = min(node,best)
-                if edges[node] != -1 and edges[node] not in node1_visited:
-                    node1_q.append(edges[node])
-            for _ in range(len(node2_q)):
-                node = node2_q.popleft()
-                node2_visited.add(node)
-                if node in node1_visited:
-                    best = min(node,best)
-                if edges[node] != -1 and edges[node] not in node2_visited:
-                    node2_q.append(edges[node])
+            node1_visited.add(node1)
+            if node1 is not None:
+                if node1 in node2_visited: best = min(node1,best)
+                if edges[node1] != -1 and edges[node1] not in node1_visited: node1 = edges[node1]
+                else: node1 = None
+            if node2 is not None:
+                node2_visited.add(node2)
+                if node2 in node1_visited: best = min(node2,best)
+                if edges[node2] != -1 and edges[node2] not in node2_visited: node2 = edges[node2]
+                else: node2 = None
 
             if best != inf:
                 return best
