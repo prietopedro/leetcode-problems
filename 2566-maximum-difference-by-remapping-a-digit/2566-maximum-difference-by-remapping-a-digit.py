234dd5ad5381aa83fa821b552dacbce88b64cefa
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2025-06-13 21:12:37 -0400

    Time: 0 ms (100%), Space: 17.9 MB (11.69%) - LeetHub

--- 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.py
+++ 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.py
@@ -1,12 +1,11 @@
 class Solution:
     def minMaxDifference(self, num: int) -> int:
-        str_arr = [num for num in str(num)]
-        max_num_replace = 0
-        while max_num_replace < len(str_arr) and str_arr[max_num_replace] == '9':
-            max_num_replace += 1
-
-        max_num = int(''.join('9' if max_num_replace < len(str_arr) and num == str_arr[max_num_replace] else num for num in str_arr))
-        min_num = int(''.join('0' if num == str_arr[0] else num for num in str_arr))
-        print(max_num,min_num)
-        return max_num - min_num
-        
+        s = str(num)
+        t = s
+        pos = 0
+        while pos < len(s) and s[pos] == "9":
+            pos += 1
+        if pos < len(s):
+            s = s.replace(s[pos], "9")
+        t = t.replace(t[0], "0")
+        return int(s) - int(t)
