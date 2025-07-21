e40121527103191a79a119bee6c8ae95f212e265
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2025-07-20 20:05:17 -0400

    Time: 395 ms (31.61%), Space: 19.7 MB (52.29%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.py
@@ -1,16 +1,9 @@
 class Solution:
     def makeFancyString(self, s: str) -> str:
-        if not s: return s
-        fancy = s[0]
-        count = 1
-        for i in range(1,len(s)):
-            if s[i] == fancy[-1] and count == 2: 
+        if len(s) <= 2: return s
+        fancy = [s[0],s[1]]
+        for i in range(2,len(s)):
+            if s[i] == fancy[-1] and s[i] == fancy[-2]: 
                 continue
-
-            if s[i] == fancy[-1]: 
-                count += 1
-            else: 
-                count = 1
-            
             fancy += s[i]
-        return fancy
+        return ''.join(fancy)
