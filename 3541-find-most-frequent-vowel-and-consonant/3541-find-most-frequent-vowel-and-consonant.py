76b20d9a22d1d59c9af83c35939e49f622676c11
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2025-09-12 20:09:11 -0500

    Time: 3 ms (65.94%), Space: 17.7 MB (89.16%) - LeetHub

--- 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.py
+++ 3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.py
@@ -1,11 +1,13 @@
 class Solution:
     def maxFreqSum(self, s: str) -> int:
         counter = Counter(s)
-        max_vowel = 0
-        max_consonant = 0
-        for key,value in counter.items():
-            if key in {'a','e','i','o','u'}:
-                max_vowel = max(max_vowel,value)
+        vowels = 'aeiou'
+        vowel_max,consonant_max = 0,0
+        counter = defaultdict(int)
+        for c in s:
+            counter[c] += 1
+            if c in vowels:
+                vowel_max = max(vowel_max,counter[c])
             else:
-                max_consonant = max(max_consonant,value)
-        return max_vowel + max_consonant
+                consonant_max = max(consonant_max,counter[c])
+        return vowel_max + consonant_max
