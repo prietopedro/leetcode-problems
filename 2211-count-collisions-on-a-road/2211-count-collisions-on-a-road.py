2d78e616aa7a26643b07d964b8e916278d0895fb
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2025-12-05 01:27:42 -0500

    Time: 87 ms (67.58%), Space: 18.2 MB (69.97%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.py
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.py
@@ -1,6 +1,22 @@
 class Solution:
     def countCollisions(self, directions: str) -> int:
-        dirs = directions.lstrip("L").rstrip("R")
-        return len(dirs) - dirs.count("S")
+        res = 0
+        flag = -1
+
+        for c in directions:
+            if c == "L":
+                if flag >= 0:
+                    res += flag + 1
+                    flag = 0
+            elif c == "S":
+                if flag > 0:
+                    res += flag
+                flag = 0
+            else:
+                if flag >= 0:
+                    flag += 1
+                else:
+                    flag = 1
+        return res
 
             
