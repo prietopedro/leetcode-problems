e4b57ceb2d45db412e54d4ec7a1c6ef9c2b1c5ec
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2025-12-05 01:27:36 -0500

    Time: 3 ms (98.63%), Space: 18.5 MB (37.54%) - LeetHub

---
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.py
@@ -0,0 +1,6 @@
+class Solution:
+    def countCollisions(self, directions: str) -> int:
+        dirs = directions.lstrip("L").rstrip("R")
+        return len(dirs) - dirs.count("S")
+
+            
