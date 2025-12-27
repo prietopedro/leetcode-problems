4069618fbbceb5cd1fbfabfee795f94f67925260
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2025-12-26 22:27:57 -0500

    Time: 244 ms (21.24%), Space: 51.1 MB (94.93%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -1,20 +1,51 @@
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        unused_rooms, used_rooms = list(range(n)), []
-        heapify(unused_rooms)
-        meeting_count = [0] * n
-        for start, end in sorted(meetings):
-            while used_rooms and used_rooms[0][0] <= start:
-                _, room = heappop(used_rooms)
-                heappush(unused_rooms, room)
-            if unused_rooms:
-                room = heappop(unused_rooms)
-                heappush(used_rooms, [end, room])
-            else:
-                room_availability_time, room = heappop(used_rooms)
-                heappush(
-                    used_rooms,
-                    [room_availability_time + end - start, room]
-                )
-            meeting_count[room] += 1
-        return meeting_count.index(max(meeting_count))
+        available_rooms = []
+        taken_rooms = []
+        for i in range(n):
+            available_rooms.append((i))
+        meetings.sort(key=lambda x: x[0])
+
+        time = 0
+        meetings_counter = defaultdict(int)
+        best_room,best_room_count = 0,0
+        times = defaultdict(int)
+        for i, (start,end) in enumerate(meetings):
+            time = start
+            while (taken_rooms and taken_rooms[0][0] <= start) or not available_rooms:
+                available_time,room = heapq.heappop(taken_rooms)
+                times[room] = available_time
+                heapq.heappush(available_rooms,room)
+
+            room = heapq.heappop(available_rooms)
+            available_time = times[room]
+            time = max(available_time,start)
+
+            meetings_counter[room] += 1
+
+            if meetings_counter[room] > best_room_count:
+                best_room,best_room_count = room,meetings_counter[room]
+            elif meetings_counter[room] == best_room_count:
+                best_room = min(best_room,room)
+
+            heapq.heappush(taken_rooms,(time + end - start, room))
+
+        return best_room
+
+        # 0,1,2,3
+        # [[2,13],[3,12],[7,10],[17,19],[18,19]]
+
+        # [2,13]
+        # {0 -> 1}
+        # 0 -> 13
+        # time = 2
+
+        # [3,12]
+        # {0 -> 1, 1 -> 1}
+        # time = 3
+
+
+
+            
+
+
