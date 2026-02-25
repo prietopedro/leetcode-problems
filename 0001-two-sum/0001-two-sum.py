0034546198ca2a0f38e5d960095391d5c1b53ef9
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-02-24 22:15:50 -0500

    Time: 0 ms (100%), Space: 20.5 MB (36.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,6 +3,6 @@ class Solution:
         seen = {}
         for i,num in enumerate(nums):
             if target - num in seen:
-                return [seen[target - num],i]
+                return [seen[target - num], i]
             seen[num] = i
         return [-1,-1]
