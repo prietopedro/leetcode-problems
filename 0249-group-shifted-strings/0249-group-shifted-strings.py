06be90633537389a023f585905963e5af0152aa1
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-03-03 17:58:15 -0500

    Time: 3 ms (43.02%), Space: 19.5 MB (41.89%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.py
+++ 0249-group-shifted-strings/0249-group-shifted-strings.py
@@ -2,12 +2,13 @@ class Solution:
     def groupStrings(self, strings: List[str]) -> List[List[str]]:
         seen = defaultdict(list)
         for i,s in enumerate(strings):
-            starting = [1]
+            starting = []
             for i in range(len(s) - 1):
                 if s[i] <= s[i + 1]:
                     starting.append(ord(s[i + 1]) - ord(s[i]))
                 else:
                     starting.append(26 - ord(s[i]) + ord(s[i + 1]))
             seen[tuple(starting)].append(s)
+        print(seen)
         return list(seen.values())
         
