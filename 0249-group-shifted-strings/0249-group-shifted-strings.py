bb16dd0f841ade479bdb291bc8874c082d7abdae
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-03-03 18:10:50 -0500

    Time: 0 ms (100%), Space: 19.3 MB (86.09%) - LeetHub

--- 0249-group-shifted-strings/0249-group-shifted-strings.py
+++ 0249-group-shifted-strings/0249-group-shifted-strings.py
@@ -4,10 +4,7 @@ class Solution:
         for i,s in enumerate(strings):
             starting = []
             for i in range(len(s) - 1):
-                if s[i] <= s[i + 1]:
-                    starting.append(ord(s[i + 1]) - ord(s[i]))
-                else:
-                    starting.append(26 - ord(s[i]) + ord(s[i + 1]))
+                starting.append((ord(s[i + 1]) - ord(s[i])) % 26)
             seen[tuple(starting)].append(s)
         return list(seen.values())
         
