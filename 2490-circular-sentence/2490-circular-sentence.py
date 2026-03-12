c2d4e28821b7036468b300d15caafd5835cb9f85
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-03-12 14:01:27 -0500

    Time: 1 ms (20.12%), Space: 19.2 MB (96.45%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.py
+++ 2490-circular-sentence/2490-circular-sentence.py
@@ -3,8 +3,6 @@ class Solution:
         if sentence[0] != sentence[-1]:
             return False
         for i in range(1,len(sentence) - 1):
-            if sentence[i] != " ":
-                continue
-            if sentence[i - 1] != sentence[i + 1]:
+            if  sentence[i] == " " and sentence[i - 1] != sentence[i + 1]:
                 return False
         return True 
