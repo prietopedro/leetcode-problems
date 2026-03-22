3f7589b76af56a375bb79b621124605368077969
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-03-22 14:15:23 -0500

    Time: 42 ms (33.77%), Space: 35.4 MB (48.92%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.py
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.py
@@ -4,7 +4,7 @@ class Solution:
         current_max = arrays[0][-1]
         best = -inf
         for i in range(1,len(arrays)):
-            best = max(best,abs(arrays[i][-1] - current_min), abs(current_max - arrays[i][0]), abs(arrays[i][-1] - current_max), abs(arrays[i][0] - current_max))
+            best = max(best,abs(arrays[i][-1] - current_min), abs(current_max - arrays[i][0]), abs(arrays[i][0] - current_max))
             current_min = min(current_min,arrays[i][0])
             current_max = max(current_max, arrays[i][-1])
         return best
