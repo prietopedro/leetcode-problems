3af004377eb21f222bb77bf07d2f9b9ba9222559
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-04-05 13:52:50 -0500

    Time: 17 ms (30.46%), Space: 19.5 MB (10.66%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,13 +1,6 @@
 class Solution:
     def judgeCircle(self, moves: str) -> bool:
-        x,y = 0,0
+        store = defaultdict(int)
         for move in moves:
-            if move == 'U':
-                x += 1
-            elif move == 'D':
-                x -= 1
-            elif move == 'L':
-                y += 1
-            else:
-                y -= 1
-        return x == 0 and y == 0
+            store[move] += 1
+        return store["U"] == store["D"] and store["L"] == store["R"]
