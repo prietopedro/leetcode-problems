a6c1c90a8b7f04cc23567ac8fdc07e668a3ae11b
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-06-08 18:49:37 -0500

    Time: 83 ms (76.25%), Space: 36.8 MB (56.25%) - LeetHub

--- 2107-number-of-unique-flavors-after-sharing-k-candies/2107-number-of-unique-flavors-after-sharing-k-candies.py
+++ 2107-number-of-unique-flavors-after-sharing-k-candies/2107-number-of-unique-flavors-after-sharing-k-candies.py
@@ -9,13 +9,10 @@ class Solution:
         for right in range(len(candies)):
             right_candy = candies[right]
             candies_dict[right_candy] -= 1
-            print(right,right_candy,candies_dict[right_candy])
             if candies_dict[right_candy] == 0:
                 distinct_candies -= 1
-            print(right, distinct_candies)
             if right - left + 1 == k:
                 left_candy = candies[left]
-                print(right)
                 best = max(best, distinct_candies)
                 candies_dict[left_candy] += 1
                 if candies_dict[left_candy] == 1:
