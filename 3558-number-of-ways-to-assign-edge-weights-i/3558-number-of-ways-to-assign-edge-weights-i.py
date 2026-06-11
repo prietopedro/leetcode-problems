97995bc021c3bf7c7fcd5ff57273bb21e1a42bb1
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-06-10 19:36:33 -0500

    Time: 293 ms (80.65%), Space: 78.8 MB (48.39%) - LeetHub

--- 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.py
+++ 3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i.py
@@ -1,29 +1,19 @@
 class Solution:
+    MOD = 10**9 + 7
+
+    def dfs(self, g: list, x: int, f: int) -> int:
+        max_dep = 0
+        for y in g[x]:
+            if y == f:
+                continue
+            max_dep = max(max_dep, self.dfs(g, y, x) + 1)
+        return max_dep
+
     def assignEdgeWeights(self, edges: List[List[int]]) -> int:
-        MOD = (10 ** 9) + 7
-        graph = defaultdict(list)
-        for x,y in edges:
-            graph[x].append(y)
-            graph[y].append(x)
-        seen = set()
-        q = [1]
-        level = -1
-        while q:
-            level += 1
-            next_level = []
-            for _ in range(len(q)):
-                curr = q.pop()
-                seen.add(curr)
-                for nei in graph[curr]:
-                    if nei not in seen:
-                        next_level.append(nei)
-            q = next_level
-        
-        return ((2 ** level) // 2) % MOD
-
-       
-
-            
-                
-                
-            
+        n = len(edges) + 1
+        g = [[] for _ in range(n + 1)]
+        for u, v in edges:
+            g[u].append(v)
+            g[v].append(u)
+        max_dep = self.dfs(g, 1, 0)
+        return pow(2, max_dep - 1, self.MOD)
