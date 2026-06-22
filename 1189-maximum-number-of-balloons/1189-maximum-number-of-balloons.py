4f171ad5cd3df6a80a02b36ba67572b23a1abe95
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-06-21 19:48:38 -0500

    Time: 1 ms (73.88%), Space: 19.2 MB (95.12%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,12 +1,11 @@
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
-        seen = [0] * 26
-        for c in text:
-            seen[ord('a') - ord(c)] += 1
-        min_char_count = inf
-        for c in "balon":
-            if c == 'l' or c == 'o':
-                min_char_count = min(min_char_count, seen[ord('a') - ord(c)] // 2)
-            else:
-                min_char_count = min(min_char_count, seen[ord('a') - ord(c)])
-        return min_char_count if min_char_count != inf else 0
+        pattern_str = 'balloon'
+        pattern_elem_freq = Counter(pattern_str)
+        text_elem_freq = Counter(text)
+        max_pattern_count = math.inf
+
+        for char in pattern_str:
+            max_pattern_count = min(max_pattern_count, text_elem_freq[char] // pattern_elem_freq[char])
+
+        return max_pattern_count
