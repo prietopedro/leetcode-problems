099e3197f6b6ee47a94047bd744374fd5a16c464
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-07-14 18:50:05 -0500

    Time: 0 ms (100%), Space: 19.2 MB (51.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,6 +1,9 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        stairs = [1,2]
+        if n <= 2: return n
+        a,b = 1,2
         for i in range(3,n + 1):
-            stairs.append(stairs[-1] + stairs[-2])
-        return stairs[n - 1]
+            c = a + b
+            a = b
+            b = c
+        return b
