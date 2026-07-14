ffe58a30ccdb9ba7b07451e7eb6cf800e65886f4
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-07-14 18:48:43 -0500

    Time: 0 ms (100%), Space: 19.2 MB (51.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,9 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        cache = {1: 1, 2: 2}
-        def rec(n, cache):
-            if n in cache:
-                return cache[n]
-            cache[n] = rec(n - 1,cache) + rec(n - 2,cache)
-            return cache[n]
-        return rec(n,cache)
+        stairs = [1,2]
+        for i in range(3,n + 1):
+            stairs.append(stairs[-1] + stairs[-2])
+        return stairs[n - 1]
