0d916c3a9c68f13f4bf56fd4e1c1795eee3a0476
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-07-18 02:33:15 -0500

    Time: 0 ms (100%), Space: 19.3 MB (70.42%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -15,14 +15,13 @@ class Solution:
         # return rec(0)  
             
 
-        dp = [0] * (len(s) + 1)
-        dp[-1] = 1
-        for i in range(len(dp) - 2, -1, -1):
-            if s[i] == '0':
-                dp[i] = 0
-            else:
-                dp[i] = dp[i + 1]
+        double, single = 0,1
+        for i in range(len(s) - 1, -1, -1):
+            current = 0
+            if s[i] != '0':
+                current = single
             
             if i + 1 < len(s) and 10 <= int(s[i:i+2]) <= 26:
-                dp[i] += dp[i + 2]
-        return dp[0]
+                current += double
+            double,single = single,current
+        return single
