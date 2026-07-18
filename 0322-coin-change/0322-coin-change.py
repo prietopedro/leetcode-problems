894581394094dacb774c8916bad7eba06d6d2ef4
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-07-18 01:27:06 -0500

    Time: 434 ms (94.44%), Space: 19.6 MB (78.51%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,17 +1,26 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        @cache
-        def minAmount(coinsLeft):
+        # @cache
+        # def minAmount(coinsLeft):
             
-            if coinsLeft == 0:
-                return 0
-            if coinsLeft < 0:
-                return inf
+        #     if coinsLeft == 0:
+        #         return 0
+        #     if coinsLeft < 0:
+        #         return inf
 
-            best = inf
-            for coin in coins:
-                best = min(best,1 + minAmount(coinsLeft - coin))
+        #     best = inf
+        #     for coin in coins:
+        #         best = min(best,1 + minAmount(coinsLeft - coin))
             
-            return best
-        best = minAmount(amount)
-        return best if best != inf else -1
+        #     return best
+        # best = minAmount(amount)
+        # return best if best != inf else -1
+
+        dp = [inf] * (amount + 1)
+        dp[0] = 0
+        for i in range(1,len(dp)):
+            for coin in coins:
+                if i < coin:
+                    continue
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+        return dp[amount] if dp[amount] != inf else -1
