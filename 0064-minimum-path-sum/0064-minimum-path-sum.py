86eb5ab1a3183ffbc517067531c1ebaee6870086
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-07-19 22:05:03 -0500

    Time: 19 ms (27.28%), Space: 21.5 MB (60.49%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,18 +1,12 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        directions_cost = [[inf for _ in range(n)] for _ in range(m)]
-        directions_cost[0][0] = grid[0][0]
-        h = [(grid[0][0],0,0)]
-        seen = set([(0,0)])
-        directions = [(0,1),(1,0)]
-        while h:
-            cost,row,col = heapq.heappop(h)
-            for xr,xc in directions:
-                nr,nc = row + xr,col + xc
-                if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] + cost < directions_cost[nr][nc]:
-                    val = grid[nr][nc] + cost
-                    directions_cost[nr][nc] = val
-                    heapq.heappush(h,(val,nr,nc))
-        return directions_cost[-1][-1]
+        m,n = len(grid),len(grid[0])
+        dp = [[inf for _ in range(n)] for _ in range(m)]
+        dp[0][0] = grid[0][0]
+        for row in range(m):
+            for col in range(n):
+                if 0 <= row - 1 < m:
+                    dp[row][col] = min(dp[row][col],dp[row - 1][col] + grid[row][col])
+                if 0 <= col - 1 < n:
+                    dp[row][col] = min(dp[row][col],dp[row][col - 1] + grid[row][col])
+        return dp[-1][-1]
