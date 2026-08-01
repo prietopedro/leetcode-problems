08dd0f4545c5f447ffdd635d0c49115adcd37b3d
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-07-31 20:09:32 -0500

    Time: 1471 ms (12.84%), Space: 519.4 MB (5.08%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,25 +1,17 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        # @cache
-        # def dp(i,left):
-        #     if left == 0:
-        #         return 1
-        #     if left < 0:
-        #         return 0
-        #     if i >= len(coins):
-        #         return 0
-        #     output = dp(i, left - coins[i]) + dp(i + 1, left)
-        #     return output
-        # return dp(0,amount)
-
-        n = len(coins)
-        dp = [[0] * (amount + 1) for _ in range(n + 1)]
-        for i in range(n + 1):
-            dp[i][0] = 1
+        # if not amount:
+        #     return 0
         
-        for i in range(n - 1, -1, -1):
-            for left in range(amount + 1):
-                take = dp[i][left - coins[i]] if left >= coins[i] else 0
-                skip = dp[i + 1][left]
-                dp[i][left] = take + skip
-        return dp[0][amount]
+        coins.sort()
+        @cache
+        def dp(i,remaining = amount):
+            if remaining == 0:
+                return 1
+            if remaining < 0:
+                return 0
+            if i >= len(coins):
+                return 0
+            output = 0
+            return dp(i, remaining - coins[i]) + dp(i + 1, remaining)
+        return dp(0)
