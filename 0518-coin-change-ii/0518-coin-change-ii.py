e60f4182f46428706fcbde3d468e3fb20c7759cf
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-07-31 20:10:03 -0500

    Time: 1676 ms (5.01%), Space: 518.8 MB (5.08%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,8 +1,5 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        # if not amount:
-        #     return 0
-        
         @cache
         def dp(i,remaining = amount):
             if remaining == 0:
@@ -11,6 +8,5 @@ class Solution:
                 return 0
             if i >= len(coins):
                 return 0
-            output = 0
             return dp(i, remaining - coins[i]) + dp(i + 1, remaining)
         return dp(0)
