22214af9e3414baa02049d636c6a68299fe912a9
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-08-15 15:35:04 -0500

    Time: 549 ms (34.68%), Space: 52.5 MB (44.51%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,11 +1,29 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        @cache
-        def dp(i, total = 0):
-            if total == amount:
-                return 1
-            if i >= len(coins) or total > amount:
-                return 0
+        # @cache
+        # def dp(i, total = 0):
+        #     if total == amount:
+        #         return 1
+        #     if i >= len(coins) or total > amount:
+        #         return 0
             
-            return dp(i, total + coins[i]) + dp(i + 1, total)
-        return dp(0)
+        #     return dp(i, total + coins[i]) + dp(i + 1, total)
+        # return dp(0)
+        n = amount + 1
+        m = len(coins) + 1
+
+        dp = [[0] * n for _ in range(m)]
+
+        # total == amount -> 1 way, regardless of i
+        for i in range(m):
+            dp[i][amount] = 1
+
+        for i in range(m - 2, -1, -1):
+            for j in range(amount - 1, -1, -1):
+                dp[i][j] = dp[i + 1][j]
+
+                if j + coins[i] <= amount:
+                    dp[i][j] += dp[i][j + coins[i]]
+
+        return dp[0][0]
+                
