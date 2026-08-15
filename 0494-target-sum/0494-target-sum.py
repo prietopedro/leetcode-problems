7302bee64c9e74bd69a555c1fc016ceabb2b2744
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-08-15 15:09:57 -0500

    Time: 453 ms (5.05%), Space: 19.5 MB (61.19%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -6,13 +6,39 @@ class Solution:
             ## return 0
         
         ## dp[i][current] = dp[i + 1][current + nums[i]] + dp[i + 1][current - nums[i]]
-        @cache
-        def dp(i, current):
-            nonlocal target
-            if i == len(nums) and current == target:
-                return 1
-            if i >= len(nums):
-                return 0
+        # @cache
+        # def dp(i, current):
+        #     nonlocal target
+        #     if i == len(nums) and current == target:
+        #         return 1
+        #     if i >= len(nums):
+        #         return 0
         
-            return dp(i + 1,current + nums[i]) + dp(i + 1,current - nums[i])
-        return dp(0,0)
+        #     return dp(i + 1,current + nums[i]) + dp(i + 1,current - nums[i])
+        # return dp(0,0)
+        total = sum(nums)
+        offset = total
+        m = 2 * total + 1
+        n = len(nums) + 1
+
+        dp = [[0] * m for _ in range(n)]
+
+        # Base case:
+        # i == len(nums)
+        # current == target -> 1 way
+        if -total <= target <= total:
+            dp[n - 1][target + offset] = 1
+
+        for i in range(n - 2, -1, -1):
+            for j in range(m):
+                current = j - offset
+
+                # current + nums[i]
+                if current + nums[i] <= total:
+                    dp[i][j] += dp[i + 1][j + nums[i]]
+
+                # current - nums[i]
+                if current - nums[i] >= -total:
+                    dp[i][j] += dp[i + 1][j - nums[i]]
+
+        return dp[0][offset]
