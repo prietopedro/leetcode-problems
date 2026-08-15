98ad897c5203ad1d327bf1bfa4f823de290cf7db
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-08-15 15:39:44 -0500

    Time: 419 ms (47.83%), Space: 19.6 MB (53.61%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -9,21 +9,24 @@ class Solution:
             
         #     return dp(i, total + coins[i]) + dp(i + 1, total)
         # return dp(0)
+
+        if amount == 0:
+            return 1
         n = amount + 1
         m = len(coins) + 1
 
-        dp = [[0] * n for _ in range(m)]
-
-        # total == amount -> 1 way, regardless of i
-        for i in range(m):
-            dp[i][amount] = 1
+        dp = [0] * n 
+        dp[amount] = 1
 
         for i in range(m - 2, -1, -1):
+            next_dp = [0] * n
             for j in range(amount - 1, -1, -1):
-                dp[i][j] = dp[i + 1][j]
+                next_dp[j] = dp[j]
+                next_dp[amount] = 1
 
                 if j + coins[i] <= amount:
-                    dp[i][j] += dp[i][j + coins[i]]
+                    next_dp[j] += next_dp[j + coins[i]]
+            dp = next_dp
 
-        return dp[0][0]
+        return dp[0]
                 
