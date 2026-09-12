8ed29f928f0224e49e60e155d964515ef2ab7f04
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-09-12 18:54:36 -0500

    Time: 4248 ms (34.28%), Space: 19.5 MB (59.08%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,28 +1,30 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        
-        def dfs(x,y,i, seen):
-            if not (0 <= x < len(board) and 0 <= y < len(board[0])):
+        rows, cols = len(board), len(board[0])
+        seen = set()
+
+        def dfs(r, c, i):
+            if not (0 <= r < rows and 0 <= c < cols):
                 return False
-            if board[x][y] != word[i]:
+            if (r, c) in seen or board[r][c] != word[i]:
                 return False
             if i == len(word) - 1:
                 return True
-            
-            seen.add((x,y))
-            nei = [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
-            for nX, nY in nei:
-                if (nX,nY) in seen:
-                    continue
-                seen.add((nX,nY))
-                res = dfs(nX,nY, i + 1, seen)
-                seen.remove((nX,nY))
-                if res:
-                    return res
-            return False
-        for row in range(len(board)):
-            for col in range(len(board[0])):
-                res = dfs(row,col, 0, set())
-                if res:
+
+            seen.add((r, c))
+
+            found = (
+                dfs(r + 1, c, i + 1)
+                or dfs(r - 1, c, i + 1)
+                or dfs(r, c + 1, i + 1)
+                or dfs(r, c - 1, i + 1)
+            )
+
+            seen.remove((r, c))
+            return found
+
+        for r in range(rows):
+            for c in range(cols):
+                if dfs(r, c, 0):
                     return True
         return False
