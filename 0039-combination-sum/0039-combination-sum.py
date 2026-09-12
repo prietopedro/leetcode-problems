a33944f8671552e10df2c9ad7bd9fba58d40586d
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-09-12 14:54:59 -0500

    Time: 11 ms (46.24%), Space: 19.5 MB (65.74%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -2,21 +2,21 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         output = []
         curr = []
-        seen = set()
         def rec(i, target):
             if i >= len(candidates) or target < 0:
                 return
             if target == 0:
-                hash_k = ','.join([str(x) for x in curr])
-                if hash_k in seen:
-                    return
-                seen.add(hash_k)
+                # hash_k = ','.join([str(x) for x in curr])
+                # if hash_k in seen:
+                #     return
+                # seen.add(hash_k)
                 output.append(curr[:])
+                return
             
+            rec(i + 1, target)
             curr.append(candidates[i])
             rec(i, target - candidates[i])
             curr.pop()
-            rec(i + 1, target)
         rec(0, target)
         return output
 
