4f14de4537bfd2e55fff6da380d4122d4e59f8ae
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-09-12 19:23:24 -0500

    Time: 39 ms (78.61%), Space: 33.6 MB (66.67%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,38 +1,22 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        #take current + partition rest
-        #if len() == 1, check next if its same, + partition rest
-        #check before and after
-
-        # 'aaa'
-        # 'a' + partition('aa')
-        # 'a' + 'a' + partition('a') 
-        # 'a' + 'a' + 'a'
-
         curr = []
         output = []
-        def backtrack(j):
-            if j >= len(s):
-                if sum(len(x) for x in curr) == len(s):
-                    output.append(curr[:])
+
+        def backtrack(start):
+            if start == len(s):
+                output.append(curr[:])
                 return
-            
-            for i in range(j, len(s)):
-                left = i
-                right = i
-                while left >= j and right < len(s) and s[left] == s[right]:
-                    curr.append(s[left:right + 1])
-                    backtrack(right + 1)
-                    curr.pop()
-                    left -= 1
-                    right += 1
-                left = i
-                right = i + 1
-                while left >= j and right < len(s) and s[left] == s[right]:
-                    curr.append(s[left:right + 1])
-                    backtrack(right + 1)
-                    curr.pop()
-                    left -= 1
-                    right += 1
+
+            for end in range(start + 1, len(s) + 1):
+                part = s[start:end]
+
+                if part != part[::-1]:
+                    continue
+
+                curr.append(part)
+                backtrack(end)
+                curr.pop()
+
         backtrack(0)
         return output
