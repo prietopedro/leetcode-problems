89a13af3cda5e794e70a7b61c3802c514360cdd9
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-09-13 10:05:53 -0500

    Time: 235 ms (78.98%), Space: 26.6 MB (26.05%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,24 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         seen = set()
-        m = len(grid)
-        n = len(grid[0])
-        def dfs(row,col):
-            for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-                nr, nc = row + dr, col + dc
-
-                if not (0 <= nr < m and 0 <= nc < n):
-                    continue
-                if grid[nr][nc] == "0" or (nr, nc) in seen:
-                    continue
-
-                seen.add((nr, nc))
-                dfs(nr, nc)
         output = 0
+        m,n = len(grid), len(grid[0])
         for row in range(m):
             for col in range(n):
                 if grid[row][col] == "1" and (row,col) not in seen:
+                    q = deque([(row,col)])
                     seen.add((row,col))
-                    dfs(row,col)
+                    while q:
+                        cr,cc = q.popleft()
+                        for nr,nc in [(cr + 1, cc), (cr - 1, cc), (cr, cc + 1), (cr, cc - 1)]:
+                            if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == "1" and (nr,nc) not in seen:
+                                seen.add((nr,nc))
+                                q.append((nr,nc))
                     output += 1
         return output
+    
