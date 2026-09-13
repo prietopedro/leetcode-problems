93e1bbbb1bb1f504a26fb78c46738023dc6293ad
Pedro Prieto <54570120+prietopedro@users.noreply.github.com>
2026-09-13 09:49:53 -0500

    Time: 270 ms (17.34%), Space: 31.3 MB (7.05%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -4,23 +4,21 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
         def dfs(row,col):
-            nonlocal m,n
-            if (row,col) in seen:
-                return
-            if not (0 <= row < m and 0 <= col < n):
-                return
-            if grid[row][col] == "0":
-                return
-            seen.add((row,col))
-            dfs(row + 1, col) 
-            dfs(row - 1, col) 
-            dfs(row, col + 1) 
-            dfs(row, col - 1)
+            for dr, dc in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+                nr, nc = row + dr, col + dc
+
+                if not (0 <= nr < m and 0 <= nc < n):
+                    continue
+                if grid[nr][nc] == "0" or (nr, nc) in seen:
+                    continue
+
+                seen.add((nr, nc))
+                dfs(nr, nc)
         output = 0
         for row in range(m):
             for col in range(n):
                 if grid[row][col] == "1" and (row,col) not in seen:
-                    print(row,col)
+                    seen.add((row,col))
                     dfs(row,col)
                     output += 1
         return output
